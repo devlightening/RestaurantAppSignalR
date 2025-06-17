fb4c20baa8887b591467fbf4894819a2856f21df
Halil Yildirim <ce.hybusiness@gmail.com>
2025-06-17 19:02:55 +0300

    AutoMapp Added.

---
+++ SignalRApi/Mapping/ContactMapping.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using SignalR.DtoLayer.ContactDto;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalRApi.Mapping
+{
+    public class ContactMapping : Profile
+    {
+        public ContactMapping()
+        {
+            CreateMap<Contact, CreateContactDto>().ReverseMap();
+            CreateMap<Contact, UpdateContactDto>().ReverseMap();
+            CreateMap<Contact, ResultContactDto>().ReverseMap();S
+            CreateMap<Contact, GetContactDto>().ReverseMap();
+
+        }
+    }
+}
