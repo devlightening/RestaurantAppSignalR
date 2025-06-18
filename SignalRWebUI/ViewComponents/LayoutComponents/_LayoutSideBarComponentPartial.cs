64f75b4ee1e1dcaf2fb7439b80d0ac5875a7c915
Halil Yildirim <ce.hybusiness@gmail.com>
2025-06-18 12:13:52 +0300

    SidebarLayout Added.

--- SignalRWebUI/ViewComponents/LayoutComponents/_LayoutSideBarComponentPartial.cs
+++ SignalRWebUI/ViewComponents/LayoutComponents/_LayoutSideBarComponentPartial.cs
@@ -2,12 +2,13 @@
 
 namespace SignalRWebUI.ViewComponents.LayoutComponents
 {
-    public class _LayoutSideBarComponentPartial : ViewComponent
+    public class _LayoutSidebarComponentPartial : ViewComponent
     {
         public IViewComponentResult Invoke()
         {
             return View();
         }
-
+        // You can add additional methods or properties here if needed
+        // For example, to pass data to the view or handle specific logic
     }
 }
