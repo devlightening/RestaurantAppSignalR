30e0cf58aec1ff40aaa64a73eba86439e0f426e6
Halil Yildirim <ce.hybusiness@gmail.com>
2025-06-23 01:47:16 +0300

    Fix: TotalAmount not updating in MoneyCase API controller; adjusted trigger logic and controller logic. 23.06

---
+++ SignalR.BusinessLayer/Abstracts/IMoneyCaseService.cs
@@ -0,0 +1,15 @@
+﻿using SignalR.DataAccessLayer.EntityFramework;
+using SignalR.EntityLayer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SignalR.BusinessLayer.Abstracts
+{
+    public interface IMoneyCaseService : IGenericService<MoneyCase>
+    {
+        public decimal TTotalMoneyCaseAmount();
+    }
+}
---
+++ SignalRApi/Controllers/MoneyCasesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SignalR.BusinessLayer.Abstracts;
+
+namespace SignalRApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoneyCasesController : ControllerBase
+    {
+        private readonly IMoneyCaseService _moneyCaseService;
+
+        public MoneyCasesController(IMoneyCaseService moneyCaseService)
+        {
+            _moneyCaseService = moneyCaseService;
+        }
+
+        [HttpGet("TotalMoneyCaseAmount")]
+        public IActionResult TotalMoneyCaseAmount()
+        {
+           var count= _moneyCaseService.TTotalMoneyCaseAmount();
+            return Ok(count);
+        }
+
+
+       
+    }
+}
